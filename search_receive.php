3e0563f91943c66a23eefcc2436d931dda7093a8
Hiroki-Fujishima <ucuc.as@gmail.com>
2020-05-13 23:32:52 +0900

    住所が出力できるように調整まで

--- search_receive.php
+++ search_receive.php
@@ -1,6 +1,6 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $id = $_POST['id'];
+    $name = $_POST['name'];
 }
 
 $dsn = 'mysql:dbname=sample;host=localhost;charset=utf8';
@@ -9,35 +9,35 @@ $password = '';
 
 $data = [];
 
-try  {
+try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT id FROM ad_address WHERE id";
+    $sql = "SELECT * FROM user";
     $stmt = $dbh->prepare($sql);
-    $stmt->bindValue(':id,','%'.$id.'%', PDO::PARAM_STR);
+    $stmt->bindValue(':name','%'.$name.'%', PDO::PARAM_STR);
     $stmt->execute();
     $count = $stmt->rowCount();
-    while($row = $stmt->fetch(PDO::ERR_ASSOC)) {
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $data[] = $row;
     }
-    echo '処理が終了しました。';
-} catch (PDOException $e) {
+
+}catch (PDOException $e){
     echo($e->getMessage());
     die();
 }
-?>
+?>ß
 <html>
 <body>
-<h1>住所検索結果</h1>
+<h1>会員データ一覧</h1>
 <p><?php echo $count;?>件見つかりました。</p>
 <table border=1>
-    <tr><th>郵便番号</th><th>住所</th></tr>
+    <tr><th>id</th><th>名前</th></tr>
     <?php foreach($data as $row): ?>
     <tr>
-    <td><?php echo $row['id']?></td>
-    <td><?php echo $row['ken_name'.'city_name'.'town_name']?></td>
+    <td><?php echo $row['id'];?></td>
+    <td><?php echo $row['name'];?></td>
     </tr>
     <?php endforeach; ?>
-    </table>
-    </body>
-    </html>
+</table>
+</body>
+</html>
